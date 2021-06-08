4d14cc37a06e7cbe44574090420845a68637c505
Roman-Daher <>
2021-06-08 14:43:14 +0000

    js fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('button').addEventListener('click', randomizer)
+let guess
+let answer
 
 function randomizer () {
   guess = parseInt(document.getElementById('val1').value)
   answer = parseInt(Math.random() * 7)
-  if (guess == answer) {
+  if (guess === answer) {
     alert('You win!')
   }
 }
